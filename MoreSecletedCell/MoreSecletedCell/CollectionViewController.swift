0c0b6042a3ba96632275ba02952acb732630dcc2
于洋 <646306366@qq.com>
2016-02-29 18:12:55 +0800

    yuyang

---
+++ MoreSecletedCell/MoreSecletedCell/CollectionViewController.swift
@@ -0,0 +1,104 @@
+//
+//  CollectionViewController.swift
+//  MoreSecletedCell
+//
+//  Created by 于洋 on 16/2/29.
+//  Copyright © 2016年 yuyang. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class CollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        self.view.backgroundColor = UIColor.redColor()
+        self.collectionView!.registerClass(MyCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+//    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+
+
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 100
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath)
+    
+       // cell.backgroundColor =  UIColor.redColor()
+        // Configure the cell
+    
+        return cell
+    }
+    
+    override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
+        let  cell:MyCell = (collectionView.cellForItemAtIndexPath(indexPath) as? MyCell)!
+        
+        cell.changeColor()
+        
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(collectionView: UICollectionView, shouldHighlightItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(collectionView: UICollectionView, shouldShowMenuForItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, canPerformAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, performAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) {
+    
+    }
+    */
+
+}
---
+++ MoreSecletedCell/MoreSecletedCell/MyCell.swift
@@ -0,0 +1,41 @@
+//
+//  MyCell.swift
+//  MoreSecletedCell
+//
+//  Created by 于洋 on 16/2/29.
+//  Copyright © 2016年 yuyang. All rights reserved.
+//
+
+import UIKit
+
+class MyCell: UICollectionViewCell {
+    
+    var  isRed:Bool?
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override  init(frame: CGRect) {
+        super.init(frame: frame)
+        pSetup()
+        isRed = true
+        
+    }
+    
+    private func pSetup(){
+        self.backgroundColor = UIColor.redColor()
+    }
+    
+    func   changeColor(){
+        if(isRed == true){
+              self.backgroundColor = UIColor.greenColor()
+            isRed = false;
+            
+        }else{
+            self.backgroundColor = UIColor.redColor()
+            isRed = true;
+        }
+    
+      
+    }
+}
